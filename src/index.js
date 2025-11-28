c58cb62b652e0d3230a15cbfb9b5cd687b52045a
Ela <ela.tech2007@gmail.com>
2025-11-28 11:26:09 +0530

    firebase  changes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyB3of8yt2TuoRdrxCuC8GTRw5GiMxXKNAI",
+    authDomain: "fir-course-acae8.firebaseapp.com",
+    projectId: "fir-course-acae8",
+    storageBucket: "fir-course-acae8.firebasestorage.app",
+    messagingSenderId: "85328427732",
+    appId: "1:85328427732:web:a3a061f8c9f8310ba89941"
+    },
+    api :{
+        createUser:""
+    }
+
+}   
